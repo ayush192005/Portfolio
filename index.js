505182177c36c747f71eca45ae6b68ba801cee08
ayush192005 <ayushbansal19505@gmail.com>
2026-07-06 19:39:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const menuButton = document.querySelector(".responsiveMenuButton");
+const menuIcon = document.querySelector(".menuIcon");
+const navLinks = document.querySelector(".navLinks");
+
+menuButton.addEventListener("click", () => {
+
+    navLinks.classList.toggle("showMenu");
+
+    if(navLinks.classList.contains("showMenu")){
+
+        menuIcon.classList.remove("ri-menu-line");
+        menuIcon.classList.add("fa-solid","fa-xmark");
+
+    }else{
+
+        menuIcon.classList.remove("fa-solid","fa-xmark");
+        menuIcon.classList.add("ri-menu-line");
+
+    }
+
+});
+
+const links = document.querySelectorAll(".navLinks a");
+
+links.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("showMenu");
+
+        menuIcon.classList.remove("fa-solid","fa-xmark");
+        menuIcon.classList.add("ri-menu-line");
+
+    });
+
+});
+document.addEventListener("DOMContentLoaded", () => {
+
+    const accordions = document.querySelectorAll(".banners");
+
+    accordions.forEach((accordion) => {
+
+        const header = accordion.querySelector(".card-header");
+        const content = accordion.querySelector(".progress-section");
+        const icon = accordion.querySelector(".down-icon");
+
+        header.addEventListener("click", () => {
+
+            // Close other accordions
+            accordions.forEach((item) => {
+
+                if (item !== accordion) {
+                    item.querySelector(".progress-section").classList.remove("active");
+                    item.querySelector(".down-icon").classList.remove("rotate");
+                }
+
+            });
+
+            // Toggle current accordion
+            content.classList.toggle("active");
+            icon.classList.toggle("rotate");
+
+        });
+
+    });
+
+});
