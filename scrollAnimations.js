39ff46bf4a7383eea7f264741512846e72f541df
ayush192005 <ayushbansal19505@gmail.com>
2026-07-10 06:19:30 +0530

    Update scrollAnimations.js

--- scrollAnimations.js
+++ scrollAnimations.js
@@ -1,11 +1,12 @@
-gsap.from(".greet, h4,",{
-    y:-30,
-    duration:0.7,
-    delay:0.5,
+var tl = document.querySelector(".navigation-section");
+
+var tl = gsap.timeline()
+
+tl.from(".navigation-section",{
+    y:-40,
     opacity:0,
-    stagger:1,
-    scrollTrigger:{
-       trigger:true,
-       
-    }
+    duration:1.3,
 })
+
+var menu = document.querySelector("responsiveNavbar i");
+var close = document.querySelector("");
