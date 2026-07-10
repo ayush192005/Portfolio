615ba84b4af2ad4c7bfc84315375dca48d02a642
ayush192005 <ayushbansal19505@gmail.com>
2026-07-10 18:14:46 +0530

    Update scrollAnimations.js

--- scrollAnimations.js
+++ scrollAnimations.js
@@ -8,5 +8,35 @@ tl.from(".navigation-section",{
     duration:1.3,
 })
 
-var menu = document.querySelector("responsiveNavbar i");
-var close = document.querySelector("");
+var menu = document.querySelector(".openIcon");
+var close = document.querySelector(".close");
+
+var nav = gsap.timeline();
+
+nav.to(".mobileNavbar",{
+    left:90,
+    duration:0.5,
+})
+
+nav.from(".mobileNavbar li",{
+    x:150,
+    duration:0.7,
+    delay:0.2,
+    opacity:0,
+    stagger:0.15,
+})
+
+nav.from(".close", {
+    opacity:0,
+})
+
+nav.pause()
+
+menu.addEventListener("click", function(){
+    nav.play()
+})
+
+close.addEventListener("click", function(){
+    nav.reverse()
+})
+
