4e169a6d2f99056aa0f6253736b241e10a41898c
ayush192005 <ayushbansal19505@gmail.com>
2026-07-11 17:33:42 +0530

    Update scrollAnimations.js

--- scrollAnimations.js
+++ scrollAnimations.js
@@ -8,34 +8,72 @@ tl.from(".navigation-section",{
     duration:1.3,
 })
 
-var menu = document.querySelector(".openIcon");
-var close = document.querySelector(".close");
+// var menu = document.querySelector(".openIcon");
+// var close = document.querySelector(".close");
 
-var nav = gsap.timeline();
+// var nav = gsap.timeline();
 
-nav.to(".mobileNavbar",{
-    left:90,
-    duration:0.5,
-})
+// nav.to(".mobileNavbar",{
+//     right:0,
+//     duration:0.5,
+// })
 
-nav.from(".mobileNavbar li",{
-    x:150,
+// nav.from(".mobileNavbar li",{
+//     x:150,
+//     duration:0.7,
+//     delay:0.2,
+//     opacity:0,
+//     stagger:0.15,
+// })
+
+// nav.from(".close", {
+//     opacity:0,
+// })
+
+// nav.pause()
+
+// menu.addEventListener("click", function(){
+//     nav.play()
+// })
+
+// close.addEventListener("click", function(){
+//     nav.reverse()
+// })
+
+
+
+
+
+
+
+
+var menu = document.querySelector(".nav i");
+var close = document.querySelector(".full i");
+
+var chac = gsap.timeline()
+
+chac.to(".full",{
+    right:0,
     duration:0.7,
-    delay:0.2,
-    opacity:0,
-    stagger:0.15,
 })
 
-nav.from(".close", {
+chac.from(".full h4",{
+   x:150,
+   duration:1,
+   stagger:0.1,
+   opacity:0,
+})
+
+chac.from(".full i",{
     opacity:0,
 })
 
-nav.pause()
+chac.pause()
 
 menu.addEventListener("click", function(){
-    nav.play()
+    chac.play()
 })
 
-close.addEventListener("click", function(){
-    nav.reverse()
+close.addEventListener("click",function(){
+    chac.reverse()
 })
